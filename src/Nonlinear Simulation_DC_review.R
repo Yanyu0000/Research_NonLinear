3bd3fe300ec66eac1ab14e12dc520a3125f0ca1f
Diego S. Cardoso <scardoso.diego@gmail.com>
2025-03-18 20:44:23 -0500

    Adding mean denominator and log-ratio approach

--- src/Nonlinear Simulation_DC_review.R
+++ src/Nonlinear Simulation_DC_review.R
@@ -23,7 +23,7 @@ n <- number*2
 alpha <- -0.5      
 
 # Intercept (to shift distribution of choices)
-beta_const <- 2.2
+beta_const <- 7.0
 
 # Apple parameters
 beta_cal_A <- -1.2       
@@ -44,9 +44,9 @@ x1 <- x2 <- x3 <- x4 <- x5 <- x6 <- x7 <- rep(NA, n)
 
 i <- 1
 while(i <= n) {
-  x1_r <- runif(1, 1, 4)            # Random price
-  x2_r <- runif(1, -2, 1)           # Normalized calorie
-  x3_r <- runif(1, -2, 1)           # Normalized size
+  x1_r <- runif(1, 1, 5)            # Random price
+  x2_r <- runif(1, -4, 3)           # Normalized calorie
+  x3_r <- runif(1, -4, 3)           # Normalized size
   x4_r <- rbinom(1, size = 1, prob = 0.5)  # Organic (binary)
   
   x1[i] <- x1_r
@@ -80,6 +80,7 @@ x7_option2 <- sample(x7)[(number+1):n]
 
 # Create dataframe
 data_juice <- data.frame(
+  ID = 1:number,
   ApplePrice = x1_option1,
   AppleCalorie = x2_option1,
   AppleSize = x3_option1,
@@ -180,13 +181,13 @@ ggsave("output/decision_plot.pdf", width = 6, height = 6)
 
 ############################## Plot Level Curves ##############################
 grid_size <- 100
-calorie_values <- seq(-2, 1, length.out = grid_size)
-size_values <- seq(-2, 1, length.out = grid_size)
+calorie_values <- seq(-4, 3, length.out = grid_size)
+size_values <- seq(-4, 3, length.out = grid_size)
 grid_data <- expand.grid(Calorie = calorie_values, Size = size_values)
 
 # Function to calculate utility (excluding price and constant)
 calculate_utility <- function(calorie, size, beta_cal, beta_size, beta_interact, organic_effect = 1) {
-  quadratic_component <- beta_cal * (calorie^2) + beta_size * (size^2) + beta_interact * calorie * size
+  quadratic_component <- beta_const + beta_cal * (calorie^2) + beta_size * (size^2) + beta_interact * calorie * size
   return(quadratic_component * organic_effect)
 }
 
@@ -210,7 +211,7 @@ grid_data_long <- melt(grid_data, id.vars = c("Calorie", "Size"),
                       variable.name = "Product", value.name = "Utility")
 
 # Create a common set of contour levels for comparison
-contour_levels <- seq(-2, 0, by = 0.25)
+contour_levels <- seq(-20, 10, by = 2)
 
 # Plot all four in one grid
 ggplot(grid_data_long, aes(x = Size, y = Calorie, z = Utility)) +
@@ -411,22 +412,22 @@ pdf("output/wtp_histograms_MNL.pdf", width = 8, height = 6)
 par(mfrow=c(2,3))
 hist(data_juice$WTP_AppleCalorie, main="WTP for Apple Calorie", xlab="WTP")
 abline(v = MNL_WTP_AppleCalorie, col = "red")
-abline(v = wtp_nonlinear_summary$Mean[1], col = "blue")
+#abline(v = wtp_nonlinear_summary$Mean[1], col = "blue")
 hist(data_juice$WTP_AppleSize, main="WTP for Apple Size", xlab="WTP")
 abline(v = MNL_WTP_AppleSize, col = "red")
-abline(v = wtp_nonlinear_summary$Mean[2], col = "blue")
+#abline(v = wtp_nonlinear_summary$Mean[2], col = "blue")
 hist(data_juice$WTP_AppleOrganic, main="WTP for Apple Organic", xlab="WTP")
 abline(v = MNL_WTP_AppleOrganic, col = "red")
-abline(v = wtp_nonlinear_summary$Mean[3], col = "blue")
+#abline(v = wtp_nonlinear_summary$Mean[3], col = "blue")
 hist(data_juice$WTP_BerryCalorie, main="WTP for Berry Calorie", xlab="WTP")
 abline(v = MNL_WTP_BerryCalorie, col = "red")
-abline(v = wtp_nonlinear_summary$Mean[4], col = "blue")
+#abline(v = wtp_nonlinear_summary$Mean[4], col = "blue")
 hist(data_juice$WTP_BerrySize, main="WTP for Berry Size", xlab="WTP")
 abline(v = MNL_WTP_BerrySize, col = "red")
-abline(v = wtp_nonlinear_summary$Mean[5], col = "blue")
+#abline(v = wtp_nonlinear_summary$Mean[5], col = "blue")
 hist(data_juice$WTP_BerryOrganic, main="WTP for Berry Organic", xlab="WTP")
 abline(v = MNL_WTP_BerryOrganic, col = "red")
-abline(v = wtp_nonlinear_summary$Mean[6], col = "blue")
+#abline(v = wtp_nonlinear_summary$Mean[6], col = "blue")
 dev.off()
 
 
@@ -438,13 +439,12 @@ data_rf <- data_juice %>%
   select(Choice, ApplePrice, AppleCalorie, AppleSize, AppleOrganic, AppleSugar, AppleRecall, ApplePackage,
          BerryPrice, BerryCalorie, BerrySize, BerryOrganic, BerrySugar, BerryRecall, BerryPackage)
 
-# Use sample splitting to address bias. First train on partition 1 (P1) and predict on P2. Then, invert it
+# Use sample splitting to address bias. First train on partition 1 (P1) and predict on P2. Then, flip it
 P1_indices <- sample(seq_len(nrow(data_rf)), size = 0.5 * nrow(data_rf))
 P1_data <- data_rf[P1_indices, ]
 P2_data <- data_rf[-P1_indices, ]
 
 P1_rf_model <- randomForest(as.factor(P1_data$Choice) ~ ., data = P1_data, ntree=1000) # Needs to tune the number of trees later
-
 P2_rf_model <- randomForest(as.factor(P2_data$Choice) ~ ., data = P2_data, ntree=1000) # Needs to tune the number of trees later
 
 # For every observation in each partition, predict the probability of each choice
@@ -473,10 +473,10 @@ print(conf_matrix_P1)
 ############################## Random Forest WTP Calculation ##############################
 
 # First, let's calculate the standard deviations we'll need for step size
-h_ApplePrice <- 0.2 * sd(data_juice$ApplePrice)
-h_BerryPrice <- 0.2 * sd(data_juice$BerryPrice)
+h_ApplePrice <- 0.1 * sd(data_juice$ApplePrice)
+h_BerryPrice <- 0.1 * sd(data_juice$BerryPrice)
 
-h_factor <- 0.2
+h_factor <- 0.1
 h_AppleCalorie <- h_factor * sd(data_juice$AppleCalorie)
 h_AppleSize <- h_factor * sd(data_juice$AppleSize)
 h_BerryCalorie <- h_factor * sd(data_juice$BerryCalorie)
@@ -499,6 +499,9 @@ calculate_rf_wtp <- function(data, model, partition_name) {
   pd_data$ApplePrice <- pd_data$ApplePrice + h_ApplePrice
   pd_probs <- predict(model, pd_data, type = "prob")
   pd_results$ApplePrice <- (pd_probs[, "Apple Juice"] - baseline_probs$prob_bl_apple)/h_ApplePrice
+  # To avoid issues with close to zero denominators, we will take the mean of the partial derivatives
+  # In doing so, we will not allow the price effect to depend on characteristics
+  pd_results$ApplePrice <- mean(pd_results$ApplePrice)
   
   # AppleCalorie
   pd_data <- original_data
@@ -532,6 +535,9 @@ calculate_rf_wtp <- function(data, model, partition_name) {
   pd_data$BerryPrice <- pd_data$BerryPrice + h_BerryPrice
   pd_probs <- predict(model, pd_data, type = "prob")
   pd_results$BerryPrice <- (pd_probs[, "Berry Juice"] - baseline_probs$prob_bl_berry)/h_BerryPrice
+  # To avoid issues with close to zero denominators, we will take the mean of the partial derivatives
+  # In doing so, we will not allow the price effect to depend on characteristics
+  pd_results$BerryPrice <- mean(pd_results$BerryPrice)
   
   # BerryCalorie
   pd_data <- original_data
@@ -572,8 +578,8 @@ calculate_rf_wtp <- function(data, model, partition_name) {
     RF_WTP_BerrySize = -pd_results$BerrySize / pd_results$BerryPrice,
     RF_WTP_BerryOrganic = -pd_results$BerryOrganic / pd_results$BerryPrice
   )
-  
-# Replace Inf and NaN with NA
+
+  # Replace Inf and NaN with NA
   for (col in names(wtp_results)) {
     wtp_results[is.infinite(wtp_results[[col]]) | is.nan(wtp_results[[col]]), col] <- NA
   }
@@ -630,6 +636,15 @@ wtp_comparison <- data.frame(
     mean(combined_true_wtp$WTP_BerryCalorie),
     mean(combined_true_wtp$WTP_BerrySize),
     mean(combined_true_wtp$WTP_BerryOrganic)
+  ),
+
+  MNL_Mean = c(
+    mean(MNL_WTP_AppleCalorie, na.rm = TRUE),
+    mean(MNL_WTP_AppleSize, na.rm = TRUE),
+    mean(MNL_WTP_AppleOrganic, na.rm = TRUE),
+    mean(MNL_WTP_BerryCalorie, na.rm = TRUE),
+    mean(MNL_WTP_BerrySize, na.rm = TRUE),
+    mean(MNL_WTP_BerryOrganic, na.rm = TRUE)
   ),
   
   RF_Mean = c(
@@ -759,3 +774,312 @@ ggplot(comparison_long, aes(x = Attribute, y = WTP, fill = Method)) +
 
 # Save in PDF
 ggsave("output/wtp_boxplot.pdf", width = 8, height = 6)
+
+
+
+##### TRYING WITH LOG APPROACH
+
+# With this approach, can use try bigger step sizes
+h_ApplePrice <- 0.3 * sd(data_juice$ApplePrice)
+h_BerryPrice <- 0.3 * sd(data_juice$BerryPrice)
+
+h_factor <- 0.3
+h_AppleCalorie <- h_factor * sd(data_juice$AppleCalorie)
+h_AppleSize <- h_factor * sd(data_juice$AppleSize)
+h_BerryCalorie <- h_factor * sd(data_juice$BerryCalorie)
+h_BerrySize <- h_factor * sd(data_juice$BerrySize)
+
+calculate_rf_wtp_logratio <- function(data, model, partition_name) {
+  # Store original data
+  original_data <- data
+  
+  # Get baseline probabilities
+  baseline_probs <- data[, paste0("prob_bl_", c("apple", "berry"))]
+  
+  # Initialize dataframes to store partial derivatives
+  pd_results <- list()
+  
+  # Create partial derivative datasets - APPLE ATTRIBUTES
+  # ApplePrice
+  pd_data <- original_data
+  pd_data$ApplePrice <- pd_data$ApplePrice + h_ApplePrice
+  pd_probs <- predict(model, pd_data, type = "prob")
+  pd_results$ApplePrice <- log(pd_probs[, "Apple Juice"]/baseline_probs$prob_bl_apple)/h_ApplePrice
+  # To avoid issues with close to zero denominators, we will take the mean of the partial derivatives
+  # In doing so, we will not allow the price effect to depend on characteristics
+  pd_results$ApplePrice <- mean(pd_results$ApplePrice)
+  
+  # AppleCalorie
+  pd_data <- original_data
+  pd_data$AppleCalorie <- pd_data$AppleCalorie + h_AppleCalorie
+  pd_probs <- predict(model, pd_data, type = "prob")
+  pd_results$AppleCalorie <- log(pd_probs[, "Apple Juice"] / baseline_probs$prob_bl_apple)/h_AppleCalorie
+  
+  # AppleSize
+  pd_data <- original_data
+  pd_data$AppleSize <- pd_data$AppleSize + h_AppleSize
+  pd_probs <- predict(model, pd_data, type = "prob")
+  pd_results$AppleSize <- log(pd_probs[, "Apple Juice"] / baseline_probs$prob_bl_apple)/h_AppleSize
+  
+  # AppleOrganic (binary - compare all 0 vs all 1)
+  # First prediction: all AppleOrganic set to 0
+  pd_data_org0 <- original_data
+  pd_data_org0$AppleOrganic <- 0
+  probs_org0 <- predict(model, pd_data_org0, type = "prob")
+
+  # Second prediction: all AppleOrganic set to 1
+  pd_data_org1 <- original_data
+  pd_data_org1$AppleOrganic <- 1
+  probs_org1 <- predict(model, pd_data_org1, type = "prob")
+
+  # Calculate effect: prob(organic=1) - prob(organic=0)
+  pd_results$AppleOrganic <- log(probs_org1[, "Apple Juice"] / probs_org0[, "Apple Juice"])
+  
+  # Create counterfactual datasets - BERRY ATTRIBUTES
+  # BerryPrice
+  pd_data <- original_data
+  pd_data$BerryPrice <- pd_data$BerryPrice + h_BerryPrice
+  pd_probs <- predict(model, pd_data, type = "prob")
+  pd_results$BerryPrice <- log(pd_probs[, "Berry Juice"] / baseline_probs$prob_bl_berry)/h_BerryPrice
+  # To avoid issues with close to zero denominators, we will take the mean of the partial derivatives
+  # In doing so, we will not allow the price effect to depend on characteristics
+  pd_results$BerryPrice <- mean(pd_results$BerryPrice)
+  
+  # BerryCalorie
+  pd_data <- original_data
+  pd_data$BerryCalorie <- pd_data$BerryCalorie + h_BerryCalorie
+  pd_probs <- predict(model, pd_data, type = "prob")
+  pd_results$BerryCalorie <- log(pd_probs[, "Berry Juice"] / baseline_probs$prob_bl_berry)/h_BerryCalorie
+  
+  # BerrySize
+  pd_data <- original_data
+  pd_data$BerrySize <- pd_data$BerrySize + h_BerrySize
+  pd_probs <- predict(model, pd_data, type = "prob")
+  pd_results$BerrySize <- log(pd_probs[, "Berry Juice"] / baseline_probs$prob_bl_berry)/h_BerrySize
+  
+  # BerryOrganic (binary - compare all 0 vs all 1)
+  # First prediction: all BerryOrganic set to 0
+  pd_data_org0 <- original_data
+  pd_data_org0$BerryOrganic <- 0
+  probs_org0 <- predict(model, pd_data_org0, type = "prob")
+
+  # Second prediction: all BerryOrganic set to 1
+  pd_data_org1 <- original_data
+  pd_data_org1$BerryOrganic <- 1
+  probs_org1 <- predict(model, pd_data_org1, type = "prob")
+
+  # Calculate effect: prob(organic=1) - prob(organic=0)
+  pd_results$BerryOrganic <- log(probs_org1[, "Berry Juice"] / probs_org0[, "Berry Juice"])
+  
+  # Calculate WTP
+  # WTP = -(marginal effect from attribute change) / (marginal effect from price change)
+  # Note: We need to handle cases where the price effect is 0 or very small
+  
+  # Convert results to a data frame
+  wtp_results <- data.frame(
+    RF_WTP_AppleCalorie = -pd_results$AppleCalorie / pd_results$ApplePrice,
+    RF_WTP_AppleSize = -pd_results$AppleSize / pd_results$ApplePrice,
+    RF_WTP_AppleOrganic = -pd_results$AppleOrganic / pd_results$ApplePrice,
+    RF_WTP_BerryCalorie = -pd_results$BerryCalorie / pd_results$BerryPrice,
+    RF_WTP_BerrySize = -pd_results$BerrySize / pd_results$BerryPrice,
+    RF_WTP_BerryOrganic = -pd_results$BerryOrganic / pd_results$BerryPrice
+  )
+
+  # Replace Inf and NaN with NA
+  for (col in names(wtp_results)) {
+    wtp_results[is.infinite(wtp_results[[col]]) | is.nan(wtp_results[[col]]), col] <- NA
+  }
+  
+  # Add partition identifier
+  wtp_results$partition <- partition_name
+  
+  return(wtp_results)
+}
+
+# Apply the function to both partitions
+P1_wtp_logr <- calculate_rf_wtp_logratio(P1_data, P2_rf_model, "P1")
+P2_wtp_logr <- calculate_rf_wtp_logratio(P2_data, P1_rf_model, "P2")
+
+# Combine the results
+rf_wtp_logr <- rbind(P1_wtp_logr, P2_wtp_logr)
+
+# Calculate summary statistics for the RF WTP values
+rf_wtp__logr_summary <- rf_wtp_logr %>%
+  summarize_at(
+    vars(starts_with("RF_WTP_")), 
+    list(Mean = ~mean(., na.rm = TRUE),
+         SD = ~sd(., na.rm = TRUE),
+         Min = ~min(., na.rm = TRUE),
+         Max = ~max(., na.rm = TRUE))
+  ) %>%
+  tidyr::pivot_longer(
+    cols = everything(),
+    names_to = c("Variable", "Statistic"),
+    names_pattern = "RF_WTP_(.+)_(.+)"
+  ) %>%
+  tidyr::pivot_wider(
+    names_from = Statistic,
+    values_from = value
+  )
+
+# Print the summary
+print(rf_wtp__logr_summary)
+
+# Create comparison dataframe
+wtp_comparison <- data.frame(
+  Attribute = c("Apple Calorie", "Apple Size", "Apple Organic", 
+                "Berry Calorie", "Berry Size", "Berry Organic"),
+  
+  True_Mean = c(
+    mean(combined_true_wtp$WTP_AppleCalorie),
+    mean(combined_true_wtp$WTP_AppleSize),
+    mean(combined_true_wtp$WTP_AppleOrganic),
+    mean(combined_true_wtp$WTP_BerryCalorie),
+    mean(combined_true_wtp$WTP_BerrySize),
+    mean(combined_true_wtp$WTP_BerryOrganic)
+  ),
+
+  MNL_Mean = c(
+    mean(MNL_WTP_AppleCalorie, na.rm = TRUE),
+    mean(MNL_WTP_AppleSize, na.rm = TRUE),
+    mean(MNL_WTP_AppleOrganic, na.rm = TRUE),
+    mean(MNL_WTP_BerryCalorie, na.rm = TRUE),
+    mean(MNL_WTP_BerrySize, na.rm = TRUE),
+    mean(MNL_WTP_BerryOrganic, na.rm = TRUE)
+  ),
+  
+  RF_Mean = c(
+    mean(rf_wtp$RF_WTP_AppleCalorie, na.rm = TRUE),
+    mean(rf_wtp$RF_WTP_AppleSize, na.rm = TRUE),
+    mean(rf_wtp$RF_WTP_AppleOrganic, na.rm = TRUE),
+    mean(rf_wtp$RF_WTP_BerryCalorie, na.rm = TRUE),
+    mean(rf_wtp$RF_WTP_BerrySize, na.rm = TRUE),
+    mean(rf_wtp$RF_WTP_BerryOrganic, na.rm = TRUE)
+  ),
+  
+  RF_Logratio_Mean = c(
+    mean(rf_wtp_logr$RF_WTP_AppleCalorie, na.rm = TRUE),
+    mean(rf_wtp_logr$RF_WTP_AppleSize, na.rm = TRUE),
+    mean(rf_wtp_logr$RF_WTP_AppleOrganic, na.rm = TRUE),
+    mean(rf_wtp_logr$RF_WTP_BerryCalorie, na.rm = TRUE),
+    mean(rf_wtp_logr$RF_WTP_BerrySize, na.rm = TRUE),
+    mean(rf_wtp_logr$RF_WTP_BerryOrganic, na.rm = TRUE)
+  )
+)
+
+print(wtp_comparison)
+
+# Combine true WTP with random forest WTP for comparison
+# First ensure we have matching rows with partition info
+rf_wtp_with_ids <- cbind(rf_wtp_logr, row_id = c(P1_indices, setdiff(1:nrow(data_juice), P1_indices)))
+true_wtp_with_ids <- cbind(true_wtp, row_id = 1:nrow(true_wtp))
+
+# Merge datasets
+comparison_data <- merge(
+  true_wtp_with_ids,
+  rf_wtp_with_ids,
+  by = "row_id"
+)
+
+# Create a long format for easier plotting
+# List of attributes to compare
+attributes <- c(
+  "AppleCalorie" = "Apple Calorie",
+  "AppleSize" = "Apple Size",
+  "AppleOrganic" = "Apple Organic",
+  "BerryCalorie" = "Berry Calorie",
+  "BerrySize" = "Berry Size",
+  "BerryOrganic" = "Berry Organic"
+)
+
+# Create comparison plots for each attribute
+plot_list <- list()
+
+for (attr_code in names(attributes)) {
+  attr_name <- attributes[attr_code]
+  true_col <- paste0("WTP_", attr_code)
+  rf_col <- paste0("RF_WTP_", attr_code)
+  
+  # Get data for this attribute
+  plot_data <- comparison_data[, c(true_col, rf_col)]
+  names(plot_data) <- c("True_WTP", "RF_WTP")
+  
+  # Remove NA/Inf values
+  plot_data <- plot_data[is.finite(plot_data$True_WTP) & 
+                          is.finite(plot_data$RF_WTP) &
+                          !is.na(plot_data$True_WTP) & 
+                          !is.na(plot_data$RF_WTP), ]
+  
+  # Create scatterplot
+  p <- ggplot(plot_data, aes(x = True_WTP, y = RF_WTP)) +
+    geom_point(alpha = 0.3) +
+    geom_abline(slope = 1, intercept = 0, color = "red", linetype = "dashed") +
+    geom_smooth(method = "loess", color = "blue") +
+    labs(
+      title = paste("True vs. RF WTP for", attr_name),
+      x = "True WTP",
+      y = "Random Forest WTP"
+    ) +
+    theme_minimal() +
+    # Add correlation coefficient
+    annotate(
+      "text", 
+      x = max(plot_data$True_WTP, na.rm = TRUE) * 0.8,
+      y = min(plot_data$RF_WTP, na.rm = TRUE) * 1.2,
+      label = paste("Correlation:", 
+                   round(cor(plot_data$True_WTP, plot_data$RF_WTP, 
+                             use = "complete.obs"), 2))
+    )
+  # Save in pdf
+  ggsave(paste0("output/", attr_code, "_comparison.pdf"), p)
+ 
+  plot_list[[attr_code]] <- p
+}
+
+# Also create boxplots comparing distributions
+comparison_long <- data.frame(
+  Attribute = character(),
+  Method = character(),
+  WTP = numeric()
+)
+
+for (attr_code in names(attributes)) {
+  attr_name <- attributes[attr_code]
+  true_col <- paste0("WTP_", attr_code)
+  rf_col <- paste0("RF_WTP_", attr_code)
+  
+  # Get data for this attribute
+  true_data <- data.frame(
+    Attribute = attr_name,
+    Method = "True",
+    WTP = comparison_data[[true_col]]
+  )
+  
+  rf_data <- data.frame(
+    Attribute = attr_name,
+    Method = "Random Forest",
+    WTP = comparison_data[[rf_col]]
+  )
+  
+  comparison_long <- rbind(comparison_long, true_data, rf_data)
+}
+
+# Remove NA/Inf values
+comparison_long <- comparison_long[is.finite(comparison_long$WTP) & !is.na(comparison_long$WTP), ]
+
+# Create boxplot
+ggplot(comparison_long, aes(x = Attribute, y = WTP, fill = Method)) +
+  geom_boxplot(alpha = 0.7) +
+  labs(
+    title = "Distribution of True vs. Random Forest WTP Estimates",
+    x = "Attribute",
+    y = "Willingness to Pay (WTP)"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1),
+    legend.position = "top"
+  ) +
+  scale_fill_manual(values = c("True" = "darkblue", "Random Forest" = "darkgreen"))
+
+
